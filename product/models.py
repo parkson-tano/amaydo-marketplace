7caf4cedd60c5d4ffa108ea2b1c5d3ba775bae6c
parkson-tano <tanoparksonsilencer@gmail.com>
2022-08-23 10:20:19 +0100

    add subcategory

--- product/models.py
+++ product/models.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.timezone import now
@@ -12,10 +13,19 @@ class Category(models.Model):
 	def __str__(self):
 		return self.title
 
+class SubCategory(models.Model):
+	category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+	title = models.CharField(max_length=200)
+	slug = AutoSlugField(populate_from='title', unique=True)
+
+	def __str__(self):
+		return self.title
+
 class Product(models.Model):
 	owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 	name = models.CharField(max_length=200)
 	category =models.ForeignKey(Category, on_delete=models.CASCADE)
+	subcategory =models.ForeignKey(SubCategory, on_delete=models.CASCADE, null=True)
 	image = models.ImageField(upload_to='products/')
 	price = models.FloatField(default=0)
 	marked_price = models.FloatField(null=True, blank=True)
